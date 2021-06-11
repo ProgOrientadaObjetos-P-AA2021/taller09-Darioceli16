ad523fb799d2cd5da25fada3651814cf83b8ad02
Darioceli16 <dc122016@gmail.com>
2021-06-10 22:22:06 -0500

    ManejoHerencia

--- ManejoHerencia/src/paquete3/PrestamoAutomovil.java
+++ ManejoHerencia/src/paquete3/PrestamoAutomovil.java
@@ -5,10 +5,83 @@
  */
 package paquete3;
 
+import paquete5.Persona;
+
 /**
  *
  * @author reroes
  */
 public class PrestamoAutomovil {
+     private String tipo;
+    private String marca;
+    private Persona garante;
+    private double valor;
+    private double valor_mensual;
+
+    public PrestamoAutomovil(Persona benef, int m, String ciud,String tip, String marc, Persona garant, double val) 
+    {
+        super(benef, m, ciud);
+        tipo=tip;
+        marca=marc;
+        garante=garant;
+        valor=val;
+    }
     
+    public void EstablecerTipo(String tip) {
+        tipo = tip;
+    }
+
+    public void EstablecerMarca(String mar) {
+        marca = mar;
+    }
+
+    public void EstablecerGarante(Persona garan) {
+        garante = garan;
+    }
+
+    
+    
+    public void EstablecerValor(double val) {
+        valor = val;
+    }
+    
+    public void EstablecerValor_mensual() {
+        valor_mensual =valor/ObtenerMeses();
+    }
+    
+    public String ObtenerTipo() {
+        return tipo;
+    }
+    
+    public String ObtenerMarca() {
+        return marca;
+    }
+    
+    public Persona ObtenerGarante() {
+        return garante;
+    }
+    
+    public double ObtenerValor() {
+        return valor;
+    }
+    public double ObtenerValor_mensual() {
+        return valor_mensual;
+    }
+   @Override
+    public String toString(){
+        String cadena = "\n*********************\nPRESTAMO AUTOMOVIL\n";
+        cadena = String.format("%sBeneficiario: %s"
+                + "\nTiempo Prestamo %d Meses"
+                + "\nCiudad: %s"
+                + "\nGarante: %s"
+                + "\nTipo: %s"
+                + "\nMarca: %s"
+                + "\nValor Auto: $%.2f"
+                + "\nValor Mensual: $%.2f", cadena,ObtenerBeneficiario().ObtenerNombre()+" "+ObtenerBeneficiario().ObtenerApellido(),
+                ObtenerMeses(),ObtenerCiudad().toLowerCase(),ObtenerGarante().ObtenerNombre()+" "+ObtenerGarante().ObtenerApellido(),
+                ObtenerTipo(),ObtenerMarca(),ObtenerValor(),ObtenerValor_mensual());
+        
+        return cadena;
+    }
 }
+
--- ManejoHerencia/src/paquete4/PrestamoEducativo.java
+++ ManejoHerencia/src/paquete4/PrestamoEducativo.java
@@ -5,10 +5,73 @@
  */
 package paquete4;
 
+import paquete5.Persona;
+
 /**
  *
  * @author reroes
  */
 public class PrestamoEducativo {
     
+    private String niveldeestudio;
+    private String centroeducativo;
+    private double valorcarrera;
+    private double valormensual;
+    
+     public PrestamoEducativo(Persona benef, int m, String ciud,String nivel, String centro, double val) 
+    {
+        super(benef, m, ciud);
+        niveldeestudio=nivel;
+        centroeducativo=centro;
+        valorcarrera=val;
+    }
+
+    public void EstablecerNiveldeestudio(String nivel) {
+        niveldeestudio = nivel;
+    }
+
+    public void EstablecerCentroeducativo(String centro) {
+        centroeducativo = centro;
+    }
+
+    public void EstablecerValorcarrera(double valor) {
+        valorcarrera = valor;
+    }
+
+    public void EstablecerValormensual() {
+        double cuota=valorcarrera/ObtenerMeses();
+        valormensual=cuota-(cuota*0.1);
+    }
+
+    public String ObtenerNiveldeestudio() {
+        return niveldeestudio;
+    }
+
+    public String ObtenerCentroeducativo() {
+        return centroeducativo;
+    }
+
+    public double ObtenerValorcarrera() {
+        return valorcarrera;
+    }
+
+    public double ObtenerValormensual() {
+        return valormensual;
+    }
+    
+    @Override
+    public String toString(){
+        String cadena = "\n*********************\nPRESTAMO EDUCATIVO\n";
+        cadena = String.format("%sEstudiante: %s"
+                + "\nTiempo Prestamo %d Meses"
+                + "\nCiudad: %s"
+                + "\nCentro Educativo: %s"
+                + "\nNivel De Estudio: %s"
+                + "\nValor Carrera: $%.2f"
+                + "\nValor Mensual: $%.2f", cadena,ObtenerBeneficiario().ObtenerNombre()+" "+ObtenerBeneficiario().ObtenerApellido(),
+                ObtenerMeses(),ObtenerCiudad().toUpperCase(),ObtenerCentroeducativo(),ObtenerNiveldeestudio(),ObtenerValorcarrera(),ObtenerValormensual());
+        
+        return cadena;
+    }
 }
+
